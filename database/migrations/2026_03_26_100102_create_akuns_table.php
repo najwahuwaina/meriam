e24e433bfe1a02558a3023e7205b1a0ea21dee38
Ridho-7 <idoyysiuu@gmail.com>
2026-04-01 19:29:09 +0700

    coba database

--- database/migrations/2026_03_26_100102_create_akuns_table.php
+++ database/migrations/2026_03_26_100102_create_akuns_table.php
@@ -9,6 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    //coba
     public function up(): void
     {
         Schema::table('akuns', function (Blueprint $table) {
