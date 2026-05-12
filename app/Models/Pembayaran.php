8ac819f05b74a0c1a54e2a88b0ca4e238e8f0f3d
unknown <julianidebora77@gmail.com>
2026-05-12 15:43:25 +0700

    update pesanan

--- app/Models/Pembayaran.php
+++ app/Models/Pembayaran.php
@@ -2,24 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pembayaran extends Model
 {
-    use HasFactory;
-
     protected $table = 'pembayaran';
 
     protected $fillable = [
         'id_pesanan',
         'tgl_bayar',
         'subtotal',
-        'ppn',
-        'total_bayar',
+        'tarif_ppn',
+        'subtotal_stlh_ppn',
+        'jumlah',
     ];
 
-    // Relasi ke Pesanan
     public function pesanan()
     {
         return $this->belongsTo(Pesanan::class, 'id_pesanan');
